4e239d764dcf5ddd880255c35e5ffa4084e40549
ge48mam <ge48mam@mytum.de>
2026-08-20 13:46:50 +0200

    Added CM Integration via Submodule

--- initial.py
+++ initial.py
@@ -1,10 +1,16 @@
+import os
 import sys
+import pm4py
 from pathlib import Path
-from pm4py.objects.log.importer.xes import importer as xes_importer
+
 from pm4py.objects.log.exporter.xes import exporter as xes_exporter
+from pm4py.objects.log.importer.xes import importer as xes_importer
 
 from log_preprocessing import preprocess
 from merge import mergeLogs
+
+sys.path.insert(0, str(Path(__file__).parent / "external" / "collaborationminer"))
+import collaboration_miner as cm
 
 
 def main():
@@ -14,6 +20,7 @@ def main():
 
     merged_log_version = 1
     logs = []
+    os.makedirs(f"{sys.argv[1]}/output", exist_ok=True)
     for f in Path(sys.argv[1]).iterdir():
         if f.is_file():
             if f.name.startswith("merged_log"):
@@ -28,10 +35,20 @@ def main():
     trace_groups = preprocess(logs)
 
     # -- merging
-    merged_Log = mergeLogs(logs, trace_groups)
+    merged_log = mergeLogs(logs, trace_groups)
 
-    xes_exporter.apply(merged_Log, f"{sys.argv[1]}/merged_log_v{merged_log_version}.xes")
+    merged_log_path = f"{sys.argv[1]}/merged_log_v{merged_log_version}.xes"
+    xes_exporter.apply(merged_log, merged_log_path)
     print("Logs merged")
+
+    # -- discover model with collaboration miner
+    cpn, return_t = cm.discover(disc_type=pm4py.discover_petri_net_inductive,
+                                path=merged_log_path,
+                                disc_parameters=None,
+                                verbose_return=True)
+
+    pm4py.save_vis_petri_net(*cpn, f"{sys.argv[1]}/output/merged_log_collab.svg")
+    print("Model discovered")
 
     # -- conformance checking
 
